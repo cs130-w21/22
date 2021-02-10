68ed631d226ba117c04b8ea26c162b2e0ca76919
Daniel Park <luckynumberseven07@gmail.com>
2021-02-09 23:15:59 -0800

    Fixed flask application to receive GET request and return a json

--- backend/Flask/api.py
+++ backend/Flask/api.py
@@ -1,9 +1,7 @@
-import flask
-import urllib.request
+from flask import Flask, jsonify, request
 import pickle
-from flask import request
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 burtWeights = None
@@ -12,17 +10,9 @@ def analyze():
     # Run analysis on received data
     return
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/api', methods=['GET'])
 def home():
-    with urllib.request.urlopen('https://github.com/cs130-w21/22/raw/master/frontend/public/index.html') as response:
-        html = response.read()
-        
-    if request.method == 'POST':
-        # If burtWeights is not none, then initialize it by importing the pickle file
-        # Run analysis on input
-        return "<h1>Placeholder</h1><p>This is output by api.py in response to a POST request.</p>"
-
-    return html
+    return jsonify({'msg':'Seems to work properly'})
 
 if __name__ == '__main__':
     app.run()
